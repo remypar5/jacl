f37f10b45bce730192a5fcc503319669c949d61c
Remy Parzinski <rs.parzinski@gmail.com>
2017-01-20 20:13:08 +0100

    Cleanup index.jsx

--- index.jsx
+++ index.jsx
@@ -1,70 +1,19 @@
+/**
+ * Use this file as a playground for your current work in progress. Do as you
+ * please, it is ignored by GIT.
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Table from './src/components/Table';
-import cx from 'classnames';
 
-const classes = ['warning', 'info', 'success', 'default'];
-const columns = [
-'lastname,firstname',
-['lastname', 'firstname'],
-{
-	id: 'Identifier',
-	lastname: 'Last Name',
-	firstname: 'First Name',
-},
-{
-	id: {
-		label: 'Identifier',
-		transform: (key, model, collection) => {
-			return `ID: ${model[key]}`;
-		},
-	},
-	lastname: {
-		label: 'Last Name',
-		transform: (key, model, collection) => {
-			return `LASTNAME: ${model[key]}`;
-		},
-	},
-	firstname: {
-		label: 'First Name',
-		transform: (key, model, collection) => {
-			return `FIRSTNAME: ${model[key]}`;
-		},
-	},
-}];
-
-const TableContainer = React.createClass({
-	generateItem(item, key, ctx) {
-		const className = cx('list-group-item', 'list-group-item-' + classes[key % classes.length]);
-		return <div key={key} className={className}>item: {item}</div>;
-	},
-
+const Container = React.createClass({
 	render() {
 		return (
-			<Table items={[{
-					id: 1,
-					lastname: 'Doe',
-					firstname: 'John',
-				}, {
-					id: 2,
-					lastname: 'Smith',
-					firstname: 'Jane',
-				}, {
-					id: 3,
-					lastname: 'Kwant, de',
-					firstname: 'Anne',
-				}, {
-					id: 4,
-					lastname: 'Parzinski',
-					firstname: 'Remy',
-				}]}
-				columns={columns[2]}
-				 />
+			// Insert component here
 		);
 	}
 });
 
 ReactDOM.render(
-	<TableContainer />,
+	<Container />,
 	document.querySelector('#app')
 );
